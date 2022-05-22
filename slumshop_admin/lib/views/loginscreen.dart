e3e05ae1358e3b0d7d26935fa5d171989e6f00bc
Fatihah Johari <teahjohari15@gmail.com>
2022-05-23 03:56:32 +0800

    recording
    7 and 8

---
+++ slumshop_admin/lib/views/loginscreen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:http/http.dart' as http;
+import 'mainscreen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late double screenHeight, screenWidth, ctrwidth;
+  bool remember = false;
+  TextEditingController emailCtrller = TextEditingController();
+  TextEditingController passwordCtrller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(32, 32, 32, 32),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                    height: screenHeight / 2.5,
+                    width: screenWidth,
+                    child: Image.asset('assets/images/1.png')),
+                const Text(
+                  "Login/Admin",
+                  style: TextStyle(fontSize: 24),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: TextField(
+                    controller: emailCtrller,
+                    decoration: InputDecoration(
+                        hintText: "Email",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                  child: TextField(
+                    controller: passwordCtrller,
+                    decoration: InputDecoration(
+                        hintText: "Password",
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Checkbox(value: remember, onChanged: _onRememberMe),
+                    const Text("Remember Me")
+                  ],
+                ),
+                SizedBox(
+                  width: screenWidth,
+                  height: 50,
+                  child: ElevatedButton(
+                    child: const Text("Login"),
+                    onPressed: _loginUser,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+
+  void _onRememberMe(bool? value) {
+    setState(() {
+      remember = value!;
+    });
+  }
+
+  void _loginUser() {
+    String _email = emailCtrller.text;
+    String _password = passwordCtrller.text;
+    if (_email.isNotEmpty && _password.isNotEmpty) {
+      http.post(
+          Uri.parse("http://10.31.156.213/slumshop/mobile/php/login_user.php"),
+          body: {"email": _email, "password": _password}).then((response) {
+        // ignore: avoid_print
+        print(response.body);
+        if (response.body == "success") {
+          Fluttertoast.showToast(
+              msg: "Success",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              fontSize: 16.0);
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (content) => const MainScreen()));
+        } else {
+          Fluttertoast.showToast(
+              msg: "Failed",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.BOTTOM,
+              timeInSecForIosWeb: 1,
+              fontSize: 16.0);
+        }
+      });
+    }
+  }
+}
